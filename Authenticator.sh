97363675180d635c83efcee9098b0f7627e09293
Freekbird (J.R) <87552496+Freekbird@users.noreply.github.com>
2026-05-08 11:12:36 -0400

    Add files via upload
    Added file deletion and integrity

--- Authenticator.sh
+++ Authenticator.sh
@@ -1,93 +1,93 @@
 #!/bin/bash
 CHOICE=$(
-whiptail --title "Log In or View Logs" --menu "Make your choice" 16 100 9 \
-	"1)" "Log into SSO Task Manager"   \
-	"2)" "Log in and View Logs"   \
-	"3)" "End script"  3>&2 2>&1 1>&3
+	whiptail --title "Log In or View Logs" --menu "Make your choice" 16 100 9 \
+		"1)" "Log into SSO Task Manager" \
+		"2)" "Log into Log Menu" \
+		"3)" "End script" 3>&2 2>&1 1>&3
 )
 
 #LOG IN SCREEN
 case $CHOICE in
-	"1)")
-USERNAME=$(whiptail --inputbox "Enter Username" 8 78 3>&1 1>&2 2>&3)
-PASSWORD=$(whiptail --passwordbox "Enter Password" 8 78 3>&1 1>&2 2>&3)
+"1)")
+	USERNAME=$(whiptail --inputbox "Enter Username" 8 78 3>&1 1>&2 2>&3)
+	PASSWORD=$(whiptail --passwordbox "Enter Password" 8 78 3>&1 1>&2 2>&3)
 
-exitstatus=$?
+	exitstatus=$?
 
-if [ $exitstatus -ne 0 ]; then
-    exit 1
-fi
+	if [ $exitstatus -ne 0 ]; then
+		exit 1
+	fi
 
-FILEUSERNAME=$(sqlite3 PASSWORDSFORSSO.db "SELECT USERNAME FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
-FILESALT=$(sqlite3 PASSWORDSFORSSO.db "SELECT SALT FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
-FILEPASSWD=$(sqlite3 PASSWORDSFORSSO.db "SELECT PASSWORD FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILEUSERNAME=$(sqlite3 PASSWORDSFORSSO.db "SELECT USERNAME FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILESALT=$(sqlite3 PASSWORDSFORSSO.db "SELECT SALT FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILEPASSWD=$(sqlite3 PASSWORDSFORSSO.db "SELECT PASSWORD FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
 
-role=$(sqlite3 PASSWORDSFORSSO.db "SELECT ADMIN FROM USERSALTPASS WHERE USERNAME='$USERNAME' LIMIT 1;")
-if [ "$role" = "1" ]; then
-	ROLE="ADMIN"
-else
-	ROLE="AUDITOR"
-fi
+	role=$(sqlite3 PASSWORDSFORSSO.db "SELECT ADMIN FROM USERSALTPASS WHERE USERNAME='$USERNAME' LIMIT 1;")
+	if [ "$role" = "1" ]; then
+		ROLE="ADMIN"
+	else
+		ROLE="AUDITOR"
+	fi
 
-if [ -z "$FILEUSERNAME" ]; then
-    whiptail --title "ERROR" --msgbox "Invalid Credentials" 8 78
-    exit 1
-fi
+	if [ -z "$FILEUSERNAME" ]; then
+		whiptail --title "ERROR" --msgbox "Invalid Credentials" 8 78
+		exit 1
+	fi
 
-PASSWORDHASH=$(echo -n "${FILESALT}${PASSWORD}" | sha256sum | awk '{print $1}')
-if [[ "$PASSWORDHASH" == "$FILEPASSWD" ]]; then
-	if [[ "$ROLE" = "ADMIN" ]]; then
-		while true; do
-			clear
-			top -b -n 1
-			echo
-			echo "Press 'p' to snapshot, 'q' to quit"
-			read -t 3 -n 1 key
-    			case "$key" in
-        			p)
+	PASSWORDHASH=$(echo -n "${FILESALT}${PASSWORD}" | sha256sum | awk '{print $1}')
+	if [[ "$PASSWORDHASH" == "$FILEPASSWD" ]]; then
+		if [[ "$ROLE" = "ADMIN" ]]; then
+			while true; do
+				clear
+				top -b -n 1
+				echo
+				echo "Press 'p' to snapshot, 'q' to quit"
+				read -t 3 -n 1 key
+				case "$key" in
+				p)
 					timestamp=$(date +%Y%m%d_%H%M%S)
 
 					FILE="snapshot_admin_${timestamp}_${FILEUSERNAME}.txt"
 
-					top -b -n 1 -c > "$FILE"
+					top -b -n 1 -c >"$FILE"
 
 					echo "Saved snapshot: $FILE"
 
 					HASH=$(sha256sum "$FILE" | awk '{print $1}')
-					echo "$FILE $HASH" >> integrity.txt
+					echo "$FILE $HASH" >>integrity.txt
 					;;
 				q)
 					break
 					;;
-			esac
-		done
-	else
-		while true; do
-			clear
-			top -b -n 1 -u !root
-			echo
-			echo "Press 'p' to take a snapshot, 'q' to quit"
-			read -t 3 -n 1 key
-			case "$key" in
+				esac
+			done
+		else
+			while true; do
+				clear
+				top -b -n 1 -u !root
+				echo
+				echo "Press 'p' to take a snapshot, 'q' to quit"
+				read -t 3 -n 1 key
+				case "$key" in
 				p)
 					timestamp=$(date +%Y%m%d_%H%M%S)
 					FILE="snapshot_auditor_${timestamp}_${FILEUSERNAME}.txt"
-					top -b -n 1 -u "!root" > "$FILE"
+					top -b -n 1 -u "!root" >"$FILE"
 					HASH=$(sha256sum "$FILE" | awk '{print $1}')
 					echo "Saved snapshot: $FILE"
-					echo "$FILE $HASH" >> integrity.txt
+					echo "$FILE $HASH" >>integrity.txt
 					;;
 				q)
 					break
 					;;
-			esac
-		done
+				esac
+			done
+		fi
+	else
+		whiptail --title "Invalid Credentials" --msgbox "Please try again" 8 78
+		exit 1
 	fi
-else
-    whiptail --title "Invalid Credentials" --msgbox "Please try again" 8 78
-    exit 1
-fi
-;;
+	;;
 esac
 
 #VIEW LOG FILES
@@ -95,80 +95,125 @@ case $CHOICE in
 
 "2)")
 
-USERNAME=$(whiptail --inputbox "Enter Username" 8 78 3>&1 1>&2 2>&3)
+	USERNAME=$(whiptail --inputbox "Enter Username" 8 78 3>&1 1>&2 2>&3)
 
-PASSWORD=$(whiptail --passwordbox "Enter Password" 8 78 3>&1 1>&2 2>&3)
+	PASSWORD=$(whiptail --passwordbox "Enter Password" 8 78 3>&1 1>&2 2>&3)
 
-exitstatus=$?
+	exitstatus=$?
 
-if [ $exitstatus -ne 0 ]; then
-    exit 1
-fi
+	if [ $exitstatus -ne 0 ]; then
+		exit 1
+	fi
 
-FILEUSERNAME=$(sqlite3 PASSWORDSFORSSO.db "SELECT USERNAME FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILEUSERNAME=$(sqlite3 PASSWORDSFORSSO.db "SELECT USERNAME FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
 
-FILESALT=$(sqlite3 PASSWORDSFORSSO.db "SELECT SALT FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILESALT=$(sqlite3 PASSWORDSFORSSO.db "SELECT SALT FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
 
-FILEPASSWD=$(sqlite3 PASSWORDSFORSSO.db "SELECT PASSWORD FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
+	FILEPASSWD=$(sqlite3 PASSWORDSFORSSO.db "SELECT PASSWORD FROM USERSALTPASS WHERE USERNAME='$USERNAME';")
 
-role=$(sqlite3 PASSWORDSFORSSO.db "SELECT ADMIN FROM USERSALTPASS WHERE USERNAME='$USERNAME' LIMIT 1;")
+	role=$(sqlite3 PASSWORDSFORSSO.db "SELECT ADMIN FROM USERSALTPASS WHERE USERNAME='$USERNAME' LIMIT 1;")
 
-if [ "$role" = "1" ]; then
-    ROLE="ADMIN"
-else
-    ROLE="AUDITOR"
-fi
+	if [ "$role" = "1" ]; then
+		ROLE="ADMIN"
+	else
+		ROLE="AUDITOR"
+	fi
 
-if [[ "$ROLE" = "ADMIN" ]]; then
+	if [[ "$ROLE" = "ADMIN" ]]; then
 
-    if [ -z "$FILEUSERNAME" ]; then
-        whiptail --title "ERROR" --msgbox "Invalid Credentials" 8 78
-        exit 1
-    fi
+		if [ -z "$FILEUSERNAME" ]; then
+			whiptail --title "ERROR" --msgbox "Invalid Credentials" 8 78
+			exit 1
+		fi
 
-    PASSWORDHASH=$(echo -n "${FILESALT}${PASSWORD}" | sha256sum | awk '{print $1}')
+		PASSWORDHASH=$(echo -n "${FILESALT}${PASSWORD}" | sha256sum | awk '{print $1}')
 
-    if [[ "$PASSWORDHASH" == "$FILEPASSWD" ]]; then
+		if [[ "$PASSWORDHASH" == "$FILEPASSWD" ]]; then
 
-        files=()
-
-        for f in snapshot_*.txt; do
-            [ -e "$f" ] || continue
-            files+=("$f" "$f")
-        done
-
-        LOGFILE=$(whiptail --title "Select Log File" --menu "Choose a log:" 20 100 10 \
-            "${files[@]}" \
-            3>&2 2>&1 1>&3)
-	
-	LOGHASH=$(sha256sum "$LOGFILE" | awk '{print $1}')
-        STORED_HASH=$(awk -v file="$LOGFILE" '$1 == file {print $2}' integrity.txt | tr -d '\n')
-
-        if [[ "$LOGHASH" == "$STORED_HASH" ]]; then
-
-            if [ -n "$LOGFILE" ]; then
-                whiptail --title "Viewing $LOGFILE" --scrolltext --textbox "$LOGFILE" 20 100
-            fi
-
-        else
-
-            whiptail --title "THIS FILE HAS BEEN TAMPERED WITH" --msgbox "THESE LOGS ARE INVALID" 40 40
-
-        fi
+			
+			CHOICE=$(
+				whiptail --title "View Logs or Delete Logs" --menu "Make your choice" 16 100 9 \
+				"1)" "View Logs" \
+					"2)" "Delete Logs" 3>&2 2>&1 1>&3
+				)
 
 
-    else
+			case $CHOICE in #Viewing Logs Section
 
-        whiptail --title "Invalid Credentials" --msgbox "Please try again" 8 78
-        exit 1
+			"1)")
 
-    fi
-fi
-;;
+			files=()
+
+			for f in snapshot_*.txt; do
+				[ -e "$f" ] || continue
+				files+=("$f" "$f")
+			done
+
+			LOGFILE=$(whiptail --title "Select Log File" --menu "Choose a log:" 20 100 10 \
+				"${files[@]}" \
+				3>&2 2>&1 1>&3)
+
+			LOGHASH=$(sha256sum "$LOGFILE" | awk '{print $1}')
+			STORED_HASH=$(awk -v file="$LOGFILE" '$1 == file {print $2}' integrity.txt | tr -d '\n')
+
+			if [[ "$LOGHASH" == "$STORED_HASH" ]]; then
+
+				if [ -n "$LOGFILE" ]; then
+					whiptail --title "Viewing $LOGFILE" --scrolltext --textbox "$LOGFILE" 20 100
+				fi
+
+			else
+
+				whiptail --title "THIS FILE HAS BEEN TAMPERED WITH" --msgbox "THESE LOGS ARE INVALID" 40 40
+
+			fi
+			;;
+
+			#Log Deleter
+
+			"2)")
+				files=()
+
+				for f in snapshot_*.txt; do
+    					[ -e "$f" ] || continue
+    					files+=("$f" "$f")
+				done
+
+				if [ ${#files[@]} -eq 0 ]; then 
+				whiptail --msgbox "No snapshot logs found" 10 60 
+				return 0 2>/dev/null || exit 0
+				fi
+
+				LOGFILE=$(whiptail --title "Select Log File to Delete" --menu "Choose a log:" 20 100 10 \
+				    "${files[@]}" \
+				    3>&2 2>&1 1>&3)
+
+					LOGHASH=$(sha256sum "$LOGFILE" | awk '{print $1}')
+					STORED_HASH=$(awk -v file="$LOGFILE" '$1 == file {print $2}' integrity.txt)
+					if [[ "$LOGHASH" != "$STORED_HASH" ]]; then
+  						whiptail --msgbox "Refusing to delete: file integrity mismatch" 10 60
+    						exit 1
+					else
+						awk -v file="$LOGFILE" '$1 != file' integrity.txt > integritytemp.txt && mv integritytemp.txt integrity.txt
+						rm -f "$LOGFILE"
+					fi
+				
+
+			;;
+			esac
+
+		else
+
+			whiptail --title "Invalid Credentials" --msgbox "Please try again" 8 78
+			exit 1
+
+		fi
+	fi
+	;;
 
 esac
 case $CHOICE in
-	"3)")
-exit
-;;
+"3)")
+	exit
+	;;
 esac
